cf47edd767c16388a2352ec020ad42f1f429df5c
Frederik Banke <frederiksbanke@gmail.com>
2022-05-19 21:14:26 +0200

    Can create new projects

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,11 @@
+import { styled, } from "src/stitches.config";
+
+export const Button = styled("button", {
+    backgroundColor: "$primary",
+    border: "none",
+    borderRadius: "5px",
+    padding: "10px",
+    color: "white",
+    fontWeight: "bold",
+    fontSize: "14px",
+});
