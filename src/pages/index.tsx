5072764225bcd7a46c8e288677b541476a85d7ec
Frederik Banke <frederiksbanke@gmail.com>
2022-05-25 18:05:51 +0200

    Change theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
+import { Row, Switch, Text, useTheme, } from "@nextui-org/react";
+import { useTheme as useNextTheme, } from "next-themes";
 import ProjectSelector from "components/projectSelector/ProjectSelector";
 import type { NextPage, } from "next";
 import Head from "next/head";
 
 const Home: NextPage = () => {
+  const { setTheme } = useNextTheme();
+  const { isDark, type } = useTheme();
   return (
     <div>
       <Head>
@@ -15,6 +19,13 @@ const Home: NextPage = () => {
         <h1>
           Mythic Trove
         </h1>
+        <Row gap={1} align="center">
+          <Text>Current theme: {type}</Text>
+          <Switch
+            checked={isDark}
+            onChange={(e) => setTheme(e.target.checked ? "dark" : "light")}
+          />
+        </Row>
         <ProjectSelector />
       </main>
 
